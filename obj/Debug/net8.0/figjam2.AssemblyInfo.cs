5d7057d82a9b916d845f98f836514352a15fe25e
Nihan <nhnarman@gmail.com>
2025-12-20 12:24:18 +0300

    v3

--- obj/Debug/net8.0/figjam2.AssemblyInfo.cs
+++ obj/Debug/net8.0/figjam2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("figjam2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b14f1d098b4bd97cd80ad0da88657a0827c8ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ef075d1b80952ed383997fb292142f0d22622f")]
 [assembly: System.Reflection.AssemblyProductAttribute("figjam2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("figjam2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
