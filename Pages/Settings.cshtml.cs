af2584fbc850836c7156501de7b729900242f6a6
Nihan <nhnarman@gmail.com>
2025-12-21 17:43:55 +0300

    son

--- Pages/Settings.cshtml.cs
+++ Pages/Settings.cshtml.cs
@@ -8,6 +8,12 @@ namespace figjam2.Pages
         public void OnGet()
         {
         }
+
+        public IActionResult OnPostUpdateSessionTimeout([FromForm] int timeoutMinutes)
+        {
+            HttpContext.Session.SetString("SessionTimeoutMinutes", timeoutMinutes.ToString());
+            return new JsonResult(new { success = true, message = "Oturum zaman aşımı ayarı güncellendi." });
+        }
     }
 }
 
